ad9594369b14e28561ecd1f1f0aa7ac4751ad41b
Abdulbas1d <muhammadaliyevabdulbosid@gmail.com>
2024-06-28 12:11:30 +0500

    The different codes writing

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const button = document.getElementById("btn");
+const buttonEl = document.getElementById("btn2");
+const button2 = document.getElementById("btn3");
+const container = document.getElementById("container")
+
+button.addEventListener('click', function() {
+    container.style.backgroundColor = "yellow"
+});
